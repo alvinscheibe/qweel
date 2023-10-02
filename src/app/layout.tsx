e42ca820042a4e0efa938943637f0660fa996fcf
Alvin Scheibe <alvin.scheibe@gmail.com>
2023-10-02 17:26:47 -0300

    Kinde auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import '@/app/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
+import Navbar from '@/components/navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,11 +15,14 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en" className={'light'}>
-      <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>{children}</body>
+      <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,35 @@
+import MaxWidthWrapper from './max-width-wrapper';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+    <nav className={'sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'}>
+      <MaxWidthWrapper>
+        <div className={'flex h-14 items-center justify-between border-b boder-zinc=200'}>
+          <Link href={'/'} className={'flex z-40 font-semibold'}>
+            <span>qweel.</span>
+          </Link>
+
+          <div className={'hidden items-center space-x-4 sm:flex'}>
+            <>
+              <Link href={'/pricing'} className={buttonVariants({ variant: 'ghost', size: 'sm' })}>
+                Pricing
+              </Link>
+              <LoginLink className={buttonVariants({ variant: 'ghost', size: 'sm' })}>
+                Sign in
+              </LoginLink>
+              <RegisterLink className={buttonVariants({ size: 'sm' })}>
+                Get started <ArrowRight className={'ml-1.5 h-5 w-5'} />
+              </RegisterLink>
+            </>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
