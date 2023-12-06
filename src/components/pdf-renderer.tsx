5be5607617f48450c9b7f77b350d68337f3d6e4b
Alvin Scheibe <alvin.scheibe@gmail.com>
2023-12-06 11:20:34 -0300

    Update pdf-renderer component

--- src/components/pdf-renderer.tsx
+++ src/components/pdf-renderer.tsx
@@ -3,7 +3,7 @@
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import { Document, Page, pdfjs } from 'react-pdf';
-import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
+import { ChevronDown, ChevronUp, Loader2, RotateCw, Search } from 'lucide-react';
 import { useToast } from '@/components/ui/use-toast';
 import { useResizeDetector } from 'react-resize-detector';
 import { Button } from '@/components/ui/button';
@@ -13,6 +13,13 @@ import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { cn } from '@/lib/utils';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import SimpleBar from 'simplebar-react';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -23,6 +30,8 @@ type PdfRendererProps = {
 const PdfRenderer = ({ url }: PdfRendererProps) => {
   const [numPages, setNumPages] = useState<number>();
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [scale, setScale] = useState<number>(1);
+  const [rotation, setRotation] = useState<number>(0);
 
   const { toast } = useToast();
   const { ref, width } = useResizeDetector();
@@ -88,27 +97,58 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
             <ChevronUp className={'h-4 w-4'} />
           </Button>
         </div>
+
+        <div className={'space-x-2'}>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button aria-label={'zoom'} variant={'ghost'} className={'gap-1.5'}>
+                <Search className={'w-4 h-4'} />
+                {scale * 100}%<ChevronDown className={'w-3 h-3 opacity-50'} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onClick={() => setScale(1)}>
+                100%
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setScale(1.5)}>
+                150%
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setScale(2)}>
+                200%
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setScale(2.5)}>
+                250%
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <Button variant={'ghost'} aria-label={'rotate 90 degrees'} onClick={() => setRotation((prev) => prev + 90)}>
+            <RotateCw className={'w-4 h-4'} />
+          </Button>
+        </div>
       </div>
 
       <div className={'flex-1 w-full max-h-screen'}>
-        <div ref={ref}>
-          <Document
-            loading={<div className={'flex justify-center'}>
-              <Loader2 className={'w-6 h-6 my-24 animate-spin'} />
-            </div>}
-            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
-            onLoadError={() => {
-              toast({
-                title: 'Error',
-                description: 'Failed to load PDF',
-                variant: 'destructive'
-              });
-            }}
-            className={'max-h-full'} file={url}
-          >
-            <Page pageNumber={currentPage} width={width? width : 1} />
-          </Document>
-        </div>
+        <SimpleBar autoHide={false} className={'max-h-[calc(100vh-10rem)]'}>
+          <div ref={ref}>
+            <Document
+              loading={<div className={'flex justify-center'}>
+                <Loader2 className={'w-6 h-6 my-24 animate-spin'} />
+              </div>}
+              onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+              onLoadError={() => {
+                toast({
+                  title: 'Error',
+                  description: 'Failed to load PDF',
+                  variant: 'destructive'
+                });
+              }}
+              className={'max-h-full'} file={url}
+            >
+              <Page pageNumber={currentPage} width={width? width : 1} scale={scale} rotate={rotation} />
+            </Document>
+          </div>
+        </SimpleBar>
       </div>
     </div>
   );
